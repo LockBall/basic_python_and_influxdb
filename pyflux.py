48bd7fd7033da88f49195e067393a91edaf82e0e
LockBall <38836349+LockBall@users.noreply.github.com>
2023-10-02 18:56:08 -0400

    delete functionality works

--- pyflux.py
+++ pyflux.py
@@ -2,65 +2,73 @@ print("\npython influx test")
 
 import os;
 import time
-import datetime;
 import random;
 import influxdb_client;
 from influxdb_client.client.write_api import SYNCHRONOUS;
 
-mybucket = "pyflux"
-myorg = "zorg"
-myurl="http://localhost:8086"
-mytoken = os.getenv('INFLUX_TOKEN')
+my_url="http://localhost:8086"
+my_token = os.getenv('INFLUX_TOKEN')
 #print(mytoken)
+my_org = "zorg"
+my_bucket = "pyflux"
+my_measurement = "weather"
+my_location = "xandria"
+
 client = influxdb_client.InfluxDBClient(
-    url=myurl,
-    token=mytoken,
-    org=myorg
+    url=my_url,
+    token=my_token,
+    org=my_org
 )
 
-data =[] #list
+data =[]
 temperature = 00.00
 lower_bound = 10
 upper_bound = 30
-point_count = 7
+point_count = 3
 
-query = "no"
-
-# Write script
-print("\ngenerate & write")
 write_api = client.write_api(write_options=SYNCHRONOUS)
+query_api = client.query_api()
+delete_api = client.delete_api()
 
-for i in range(point_count):
-        temperature = round(random.uniform(lower_bound, upper_bound), 2)
-        relative_humidity = round(random.uniform(lower_bound, upper_bound), 2)
-        point = influxdb_client\
-                .Point("weather")\
-                .tag("location", "Prague")\
-                .field("temperature", temperature)\
-                .field("relative_humidity", relative_humidity)\
-                .time(time.time_ns())
-        data.append(point)
-        #write_api.write(bucket=mybucket, org=myorg, record=point)
-        #time.sleep(10)
+write = "yes"
+query = "yes"
+delete = "no"
 
-for element in data:
-        print(element)
+# BEGIN Write
+if write == "yes":
+        print("\ngenerate & write")
 
-write_api.write(bucket=mybucket, record=data)
-write_api.close
+        for i in range(point_count):
+                temperature = round(random.uniform(lower_bound, upper_bound), 2)
+                relative_humidity = round(random.uniform(lower_bound, upper_bound), 2)
+                point = influxdb_client\
+                        .Point(my_measurement)\
+                        .tag("location", my_location)\
+                        .field("temperature", temperature)\
+                        .field("relative_humidity", relative_humidity)\
+                        .time(time.time_ns())
+                data.append(point)
+                #write_api.write(bucket=mybucket, org=myorg, record=point)
+                #time.sleep(10)
 
-# Query script
+        for element in data:
+                print(element)
 
+        write_api.write(bucket=my_bucket, record=data)
+        write_api.close
+# END Write
+
+
+# BEGIN Query
 if query == "yes":
         print("\nquery")
-        query_api = client.query_api()
         query = 'from(bucket:"pyflux")\
         |> range(start: -10m)\
         |> filter(fn:(r) => r._measurement == "weather")\
-        |> filter(fn:(r) => r.location == "Prague")\
+        |> filter(fn:(r) => r.location == "xandria")\
         |> filter(fn:(r) => r._field == "temperature")'
 
-        result = query_api.query(org=myorg, query=query)
+        result = query_api.query(org=my_org, query=query)
         results = []
 
         for table in result:
@@ -69,5 +77,12 @@ if query == "yes":
 
         print(results)
 # end if query == "yes"
+
+#BEGIN Delete
+if delete == "yes":
+        start = "1970-01-01T00:00:00Z"
+        stop = "2023-11-01T00:00:00Z"
+        delete_api.delete(start, stop, '_measurement="weather"', bucket=my_bucket, org=my_org)
+# END Delete
 
 client.close()
